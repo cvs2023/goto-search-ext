71a334564811ab288e6731e5fc4bd505174376b3
cvs2023 <cvtheking464@gmail.com>
2023-07-11 00:02:08 +0530

    text is redirected with google link and link is working on its own

--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   var searchButtons = document.getElementsByClassName("searchButton");
+  function handleSearchGoogle() {
+    var searchText = this.innerText;
+    var searchQuery = encodeURIComponent(searchText);
+    var searchURL = "https://www.google.com/search?q=" + searchQuery;
+    window.open(searchURL, "_blank");
+  }
 
   Array.from(searchButtons).forEach(function (button) {
-    button.addEventListener("click", function () {
-      var searchText = button.innerText;
-      var searchQuery = encodeURIComponent(searchText);
-      var searchURL = "https://www.google.com/search?q=" + searchQuery;
-      chrome.tabs.create({ url: searchURL });
-    });
+    button.addEventListener("click", handleSearchGoogle);
   });
 
   var inputElement = document.getElementById("queryText");
@@ -15,13 +16,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
   addBtn.addEventListener("click", function () {
     var list = document.getElementById("list");
+    var input = inputElement.value;
+    var linkRegex =
+      /^(https?:\/\/)?([\w-]+(\.[\w-]+)+\/?)([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])?$/;
 
-    var element = document.createElement("p");
-    element.classList.add("searchButton");
-    element.innerText = inputElement.value;
+    if (linkRegex.test(input)) {
+      var myAnchor = document.createElement("a");
+      myAnchor.classList.add("searchButton");
+      myAnchor.setAttribute("href", inputElement.value);
+      myAnchor.setAttribute("target", "blank");
+      myAnchor.textContent = input;
+      list.append(myAnchor);
+    } else {
+      var element = document.createElement("a");
+      element.classList.add("searchButton");
+      element.innerText = input;
 
-    list.append(element);
+      element.addEventListener("click", handleSearchGoogle);
 
-    console.log("clicked");
+      list.append(element);
+    }
   });
 });
